4ab9bb122a51b0606dee1f16c1a88e11ee7f3020
AkaraChen <akarachen@outlook.com>
2025-03-28 16:55:10 +0800

    feat(file-item.vue): add dropdown menu for additional file options and prevent click event when disabled

--- components/editor/file-item.vue
+++ components/editor/file-item.vue
@@ -6,10 +6,21 @@
         active: isActive,
         'cursor-not-allowed opacity-70': disabled,
       }"
-      @click="$emit('click')"
+      @click="disabled ? null : $emit('click')"
     >
       <Icon name="mdi:file-outline" size="16" />
       {{ fileName }}
+      <details class="dropdown ml-auto">
+        <summary class="btn btn-ghost btn-square btn-xs">
+          <Icon name="mdi:more-vert" size="16"/>
+        </summary>
+        <ul
+          class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+        >
+          <li><a>Item 1</a></li>
+          <li><a>Item 2</a></li>
+        </ul>
+      </details>
     </a>
   </li>
 </template>
